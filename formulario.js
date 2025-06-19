59ae3314c231a4997d23e0c88d0a5bf179e746bf
wangdrely <wangdrelyterrero@gmail.com>
2025-06-19 10:13:02 -0400

    Add files via upload

---
+++ formulario.js
@@ -0,0 +1,4 @@
+document.getElementById('contact-formwang').addEventListener('submit', function(event) {
+    event.preventDefault();
+    alert('¡Gracias por ponerte en contacto! Te responderemos pronto.');
+});
